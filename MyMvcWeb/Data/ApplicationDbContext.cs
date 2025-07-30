f393a47bb101cdce024227e5e8d3edb1cd55abc7
ashokpnd12 <ashokpnd12@gmail.com>
2025-07-30 14:03:29 +0530

    Initial Commit

---
+++ MyMvcWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMvcWeb.Models;
+
+namespace MyMvcWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<CategoryModel> Categories { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<MyMvcWeb.Models.CategoryModel>().HasData(
+        //        new MyMvcWeb.Models.CategoryModel
+        //        {
+        //            Id = 1,
+        //            Name = "Action",
+        //            DisplayOrder = 1
+        //        },
+        //        new MyMvcWeb.Models.CategoryModel
+        //        {
+        //            Id = 2,
+        //            Name = "Comedy",
+        //            DisplayOrder = 2
+        //        }
+        //    );
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
