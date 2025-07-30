1ae5652d357448bf5d49a69281bb3d0a93751c9c
ashokpnd12 <ashokpnd12@gmail.com>
2025-07-30 11:58:17 +0530

    Add project files.

---
+++ MyMvcWeb/Models/CategoryModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyMvcWeb.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
